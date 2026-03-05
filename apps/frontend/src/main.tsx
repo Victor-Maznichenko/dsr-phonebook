7cd5b4b9f7532658b22c9751408b191a15251919
Victor Maznichenko <victor_maznichenko@mail.ru>
2026-03-06 02:28:37 +0300

    feat[frontend]: add API generation scripts and update dependencies

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './app';
+import '@/shared/api/setup';
 import '@/shared/styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
