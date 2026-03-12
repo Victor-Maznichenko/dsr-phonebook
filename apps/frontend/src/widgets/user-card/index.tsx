a302b086a51ff64ef371820d86b496f712a46fd4
Victor Maznichenko <victor_maznichenko@mail.ru>
2026-03-12 23:10:42 +0300

    fix[frontend]: corrected grade and department labels

--- apps/frontend/src/widgets/user-card/index.tsx
+++ apps/frontend/src/widgets/user-card/index.tsx
@@ -1,7 +1,9 @@
+import type { DepartmentValue, GradeValue } from '@/shared/lib';
 import type { ComponentProps } from 'react';
 import { Link } from 'atomic-router-react';
 import clsx from 'clsx';
 import { routes } from '@/shared/config';
+import { DEPARTMENTS_LABELS, GRADE_LABELS } from '@/shared/lib';
 import { Avatar, Button, Typography } from '@/shared/ui';
 import styles from './styles.module.scss';
 
@@ -15,7 +17,7 @@ export const UserCard = ({ className, id, avatar, firstName, lastName, grade, de
       <Avatar className={styles.avatar} text={fullName} src={avatar} size={100} />
       <div>
         <Typography variant='heading_4' as='h4'>{fullName}</Typography>
-        <Typography variant='text_S'>{grade}, {department}</Typography>
+        <Typography variant='text_S'>{GRADE_LABELS[grade as GradeValue]}, {DEPARTMENTS_LABELS[department as DepartmentValue]}</Typography>
       </div>
       <Button params={{ userId: String(id) }} variant='light-orange-md' to={routes.user as any} as={Link}>Посмотреть</Button>
     </article>
